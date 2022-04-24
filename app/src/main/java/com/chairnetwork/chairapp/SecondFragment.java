4068384195dc48392aed1fbfeff40952a80340f9
evans-tim <timjevans2000@gmail.com>
2022-04-24 13:37:29 -0500

    all 5 chairs can change color

--- app/src/main/java/com/chairnetwork/chairapp/SecondFragment.java
+++ app/src/main/java/com/chairnetwork/chairapp/SecondFragment.java
@@ -72,6 +72,9 @@ public class SecondFragment extends Fragment {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Weight_data").child("1-setFloat");
         DatabaseReference myRef2 = database.getReference("Weight_data").child("2-setFloat");
+        DatabaseReference myRef3 = database.getReference("Weight_data").child("3-setFloat");
+        DatabaseReference myRef4 = database.getReference("Weight_data").child("4-setFloat");
+        DatabaseReference myRef5 = database.getReference("Weight_data").child("5-setFloat");
 
         //myRef.setValue(1511);
 
@@ -123,6 +126,93 @@ public class SecondFragment extends Fragment {
                 }
                 else{
                     Button chair1 = getView().findViewById(R.id.button4);
+                    chair1.setTextColor(getResources().getColor(R.color.black));
+                    chair1.setBackgroundColor(getResources().getColor(R.color.hawkeye_gold));
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.print("error");
+            }
+        });
+
+        // Read from the database
+        myRef3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Integer value = dataSnapshot.getValue(Integer.class);
+                System.err.println("Database Value:");
+                System.err.println(value);
+                System.err.println("End Database Value");
+                ((TextView)getView().findViewById(R.id.db_output)).setText(String.valueOf(value));
+
+                if(value > 1000){
+                    Button chair1 = getView().findViewById(R.id.button5);
+                    chair1.setTextColor(getResources().getColor(R.color.hawkeye_gold));
+                    chair1.setBackgroundColor(getResources().getColor(R.color.black));
+                }
+                else{
+                    Button chair1 = getView().findViewById(R.id.button5);
+                    chair1.setTextColor(getResources().getColor(R.color.black));
+                    chair1.setBackgroundColor(getResources().getColor(R.color.hawkeye_gold));
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.print("error");
+            }
+        });
+
+        // Read from the database
+        myRef4.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Integer value = dataSnapshot.getValue(Integer.class);
+                System.err.println("Database Value:");
+                System.err.println(value);
+                System.err.println("End Database Value");
+                ((TextView)getView().findViewById(R.id.db_output)).setText(String.valueOf(value));
+
+                if(value > 1000){
+                    Button chair1 = getView().findViewById(R.id.button6);
+                    chair1.setTextColor(getResources().getColor(R.color.hawkeye_gold));
+                    chair1.setBackgroundColor(getResources().getColor(R.color.black));
+                }
+                else{
+                    Button chair1 = getView().findViewById(R.id.button6);
+                    chair1.setTextColor(getResources().getColor(R.color.black));
+                    chair1.setBackgroundColor(getResources().getColor(R.color.hawkeye_gold));
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.print("error");
+            }
+        });
+
+        // Read from the database
+        myRef5.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Integer value = dataSnapshot.getValue(Integer.class);
+                System.err.println("Database Value:");
+                System.err.println(value);
+                System.err.println("End Database Value");
+                ((TextView)getView().findViewById(R.id.db_output)).setText(String.valueOf(value));
+
+                if(value > 1000){
+                    Button chair1 = getView().findViewById(R.id.button7);
+                    chair1.setTextColor(getResources().getColor(R.color.hawkeye_gold));
+                    chair1.setBackgroundColor(getResources().getColor(R.color.black));
+                }
+                else{
+                    Button chair1 = getView().findViewById(R.id.button7);
                     chair1.setTextColor(getResources().getColor(R.color.black));
                     chair1.setBackgroundColor(getResources().getColor(R.color.hawkeye_gold));
                 }
