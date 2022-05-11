11c00538498e6c983d61eeedc9543a6cb2753ba6
evans-tim <timjevans2000@gmail.com>
2022-05-11 15:04:06 -0500

    check for reservation conflicts, display current reservations in AM/PM form

--- app/src/main/java/com/chairnetwork/chairapp/Reserve.java
+++ app/src/main/java/com/chairnetwork/chairapp/Reserve.java
@@ -1,5 +1,7 @@
 package com.chairnetwork.chairapp;
 
+import java.time.LocalDateTime;
+
 import androidx.annotation.NonNull;
 
 import android.content.Context;
@@ -9,6 +11,9 @@ import com.chairnetwork.chairapp.databinding.FragmentFirstBinding;
 import com.chairnetwork.chairapp.databinding.ReserveBinding;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -39,6 +44,44 @@ public class Reserve extends Fragment {
         System.err.println("CHANGING CHAIR NUM TO " + chairNumber);
     }
 
+    public static String convert24HourToAmPm(String time) {
+
+        if (time == null)
+            return time;
+
+        // Convert time where time is like: 0100, 0200, 0300....2300...
+        if (time.length() == 4) {
+            String hour = time.substring(0,2);
+            String minutes = time.substring(2,4);
+            String meridian = "am";
+
+            if (hour.substring(0,2).equals("00")) {
+                hour = "12";
+            } else if (hour.substring(0,1).equals("1") || hour.substring(0,1).equals("2")) {
+                meridian = "pm";
+                Integer militaryHour = Integer.parseInt(hour);
+                Integer convertedHour = null;
+
+                if (militaryHour > 12) {
+                    convertedHour = (militaryHour - 12);
+
+                    if (convertedHour < 10)
+                        hour = "0" + String.valueOf(convertedHour);
+                    else
+                        hour = String.valueOf(convertedHour);
+                }
+            }
+
+            time = hour + ":" + minutes + " " + meridian;
+        }
+
+        if(time.charAt(0) == '0'){
+            time = time.substring(1);
+        }
+
+        return time;
+    }
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -53,6 +96,36 @@ public class Reserve extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        MainActivity main = (MainActivity) getActivity();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        System.err.println("CHAIR NUM IS::: " + main.chairNum);
+        myRef = database.getReference("reservations").child(main.chairNum).child("2");
+
+        ValueEventListener valueEventListener2 = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                TextView view2 = ((TextView)getView().findViewById(R.id.textView));
+                String reservations = dataSnapshot.getValue(String.class);
+                String[] reservationList = reservations.split(",");
+                for(String timeSlot : reservationList) {
+                    if(timeSlot.length() > 0) {
+                        String[] startAndEndTime= timeSlot.split("-");
+                        String startString = startAndEndTime[0];
+                        String endString = startAndEndTime[1];
+                        view2.setText(String.valueOf(view2.getText() + convert24HourToAmPm(startString) + " - " + convert24HourToAmPm(endString) +  "\n"));
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.print("error");
+            }
+        };
+        myRef.addListenerForSingleValueEvent(valueEventListener2);
+
+
         binding.buttonPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,11 +138,7 @@ public class Reserve extends Fragment {
             @Override
             public void onClick(View view) {
 
-                MainActivity main = (MainActivity) getActivity();
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                System.err.println("CHAIR NUM IS::: " + chairNumber);
-                myRef = database.getReference("reservations").child(main.chairNum).child("1");
-                System.err.println(myRef.orderByValue());
+
                 Button startTime = getView().findViewById(R.id.button_startTime);
                 Button endTime = getView().findViewById(R.id.button_endTime);
                 CharSequence startNum = startTime.getText();
@@ -77,23 +146,69 @@ public class Reserve extends Fragment {
                 String start = "" + startNum.charAt(0) + startNum.charAt(1) + startNum.charAt(3) + startNum.charAt(4);
                 String end = "" + endNum.charAt(0) + endNum.charAt(1) + endNum.charAt(3) + endNum.charAt(4);
 
-                int startInt = Integer.parseInt(start);
-                int endInt = Integer.parseInt(end);
-                if(startInt < endInt){
-                    if(endInt-startInt < 400){
-                        String store = start + "-" + end;
-                        System.err.println(store);
-                        myRef.setValue(store);
-                    }
+                if(!startNum.toString().equals("Select Start Time") && !endNum.toString().equals("Select End Time")){
+                    int startInt = Integer.parseInt(start);
+                    int endInt = Integer.parseInt(end);
+
+
+                    ValueEventListener valueEventListener = new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            String reservations = dataSnapshot.getValue(String.class);
+                            System.out.println("RESERVATIONS" + reservations);
+
+                            Boolean isConflicting = false;
+
+                            String[] reservationList = reservations.split(",");
+                            for(String timeSlot : reservationList){
+                                if(timeSlot.length() > 0){
+                                    String[] startAndEndTime= timeSlot.split("-");
+                                    String startString = startAndEndTime[0];
+                                    String endString = startAndEndTime[1];
+                                    int startInt2 = Integer.parseInt(startString);
+                                    int endInt2 = Integer.parseInt(endString);
+                                    if(startInt >= startInt2 && startInt < endInt2){
+                                        System.err.println("OVERLAP with" + startString + "-" + endString);
+                                        isConflicting = true;
+                                    }
+                                    else if(endInt > startInt2 && endInt <= endInt2){
+                                        System.err.println("OVERLAP with" + startString + "-" + endString);
+                                        isConflicting = true;
+                                    }
+                                }
+                            }
+                            TextView view2 = ((TextView)getView().findViewById(R.id.textView));
+                            if(startInt < endInt && !isConflicting){
+                                if(endInt-startInt < 400){
+                                    System.err.println("setting db");
+                                    String store = start + "-" + end;
+//                        System.err.println(store);
+                                    myRef.setValue(reservations + "," + store);
+
+                                    view2.setText(String.valueOf(view2.getText() + convert24HourToAmPm(start) + " - " + convert24HourToAmPm(end) +  "\n"));
+
+
+                                }
+                            }
+                            else{
+                                view2.setText(String.valueOf(view2.getText()+ "Conflict" + "\n"));
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            System.out.print("error");
+                        }
+                    };
+                    myRef.addListenerForSingleValueEvent(valueEventListener);
                 }
-                TextView view2 = ((TextView)getView().findViewById(R.id.textView)); //
-                view2.setText(String.valueOf(view2.getText()+ "\n"+startNum.charAt(1)+startNum.charAt(2)+startNum.charAt(3)+startNum.charAt(4)+"AM-"+endNum.charAt(1)+endNum.charAt(2)+endNum.charAt(3)+endNum.charAt(4) + "AM"));
-                System.err.println(startInt);
-                System.err.println(endInt);
-                System.err.println(startTime.getText());
-                System.err.println(endTime.getText());
+
+
+
             }
         });
+
     }
 
     @Override
